d119fbd535fc136416f700f55230dbc9af3255e4
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-13 16:23:07 +0200

    Fixed eslint issues
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/api/routers/contact.ts
+++ src/server/api/routers/contact.ts
@@ -4,28 +4,48 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 export const contactRouter = createTRPCRouter({
   createContact: protectedProcedure
     .input(z.object({ contactId: z.string(), ownerId: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.db.contact.create({
+    .query(async ({ ctx, input }) => {
+      // Add contact twice
+      await ctx.db.contact.create({
         data: {
           ownerId: ctx.session.user.id,
           contactId: input.contactId,
         },
       });
+      await ctx.db.contact.create({
+        data: {
+          ownerId: input.contactId,
+          contactId: ctx.session.user.id,
+        },
+      });
+      return { message: "Created" };
     }),
 
   deleteContact: protectedProcedure
     .input(z.object({ contactId: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.db.contact.delete({
+    .query(async ({ ctx, input }) => {
+      // Delete contact twice
+      await ctx.db.contact.delete({
         where: {
-          contactId: input.contactId,
-          ownerId: ctx.session.user.id,
+          ownerId_contactId: {
+            contactId: input.contactId,
+            ownerId: ctx.session.user.id,
+          },
         },
       });
+      await ctx.db.contact.delete({
+        where: {
+          ownerId_contactId: {
+            contactId: ctx.session.user.id,
+            ownerId: input.contactId,
+          },
+        },
+      });
+      return { message: "Deleted" };
     }),
 
-  getContacts: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.contact.findMany({
+  getContacts: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.contact.findMany({
       where: {
         ownerId: ctx.session.user.id,
       },

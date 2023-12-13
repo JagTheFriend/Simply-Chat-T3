f2ac1d2e9753cc98cd2f10b42d270ffaa56761d0
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-13 15:41:42 +0200

    Added function to delete contact
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/api/routers/contact.ts
+++ src/server/api/routers/contact.ts
@@ -2,13 +2,24 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const contactRouter = createTRPCRouter({
-  getLatest: protectedProcedure
+  createContact: protectedProcedure
     .input(z.object({ contactId: z.string(), ownerId: z.string() }))
     .query(({ ctx, input }) => {
       return ctx.db.contact.create({
         data: {
-          ownerId: input.ownerId,
+          ownerId: ctx.session.user.id,
           contactId: input.contactId,
+        },
+      });
+    }),
+
+  deleteContact: protectedProcedure
+    .input(z.object({ contactId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.contact.delete({
+        where: {
+          contactId: input.contactId,
+          ownerId: ctx.session.user.id,
         },
       });
     }),

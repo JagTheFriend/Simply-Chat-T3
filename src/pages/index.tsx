47de2194b6ce262938654a7614c93f0176aa2e9b
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-13 14:06:20 +0200

    Extract metadata into another component
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import Head from "next/head";
 import Navbar from "~/components/Navbar";
 
+function Metadata() {
+  return (
+    <Head>
+      <title>Simply Chat</title>
+      <meta
+        name="description"
+        content="A Simple Chat App created using create-t3-app"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Simply Chat</title>
-        <meta
-          name="description"
-          content="A Simple Chat App created using create-t3-app"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Metadata />
       <Navbar />
     </>
   );

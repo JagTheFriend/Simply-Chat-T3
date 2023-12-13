e1527c8373cfd006aa0eb25676cf7373cce56b0c
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-13 15:37:22 +0200

    Created new route
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/server/api/routers/contact.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const contactRouter = createTRPCRouter({
+  getLatest: protectedProcedure
+    .input(z.object({ contactId: z.string(), ownerId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.contact.create({
+        data: {
+          ownerId: input.ownerId,
+          contactId: input.contactId,
+        },
+      });
+    }),
+});

524cbe5727c43ae90408eeba49572751d79e4d9f
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-13 14:01:50 +0200

    Basic navbar
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function Navbar() {
+  const { data: currentSessionData } = useSession();
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Simply Chat
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+                onClick={() =>
+                  currentSessionData ? void signOut() : void signIn()
+                }
+              >
+                {currentSessionData ? "Sign Out" : "Sign In"}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}

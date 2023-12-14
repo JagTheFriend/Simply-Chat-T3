15973136fe1c0ca57400ad756b1fb274a7781f15
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-14 11:03:24 +0200

    Updated imports
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import { signIn, useSession } from "next-auth/react";
 import Head from "next/head";
+import Image from "next/image";
+import { AddNewContact, DisplayContactsList } from "~/components/Contact";
 import Navbar from "~/components/Navbar";
-import { api } from "~/utils/api";
 
 function Metadata() {
   return (
@@ -32,7 +32,7 @@ function DisplayUsername() {
       {currentSessionData ? (
         <>
           Welcome
-          <img
+          <Image
             alt="Avatar"
             className="avatar avatar-48 bg-light rounded-circle text-white p-2"
             src={currentSessionData?.user.image ?? ""}
@@ -46,65 +46,6 @@ function DisplayUsername() {
         </button>
       )}
     </div>
-  );
-}
-
-function DisplayContactsList() {
-  const { data: contactData, isError } = api.contact.getContacts.useQuery();
-
-  if (isError) return <>An Error occurred while loading all contacts</>;
-
-  return (
-    <ul className="list-group">
-      {contactData?.map((contact) => {
-        const { data: userData, isError: fetchError } =
-          api.user.findUser.useQuery({
-            userId: contact.contactId,
-          });
-        if (fetchError)
-          return <li className="list-group-item">Unable to load info</li>;
-        return (
-          <li className="list-group-item">
-            <img
-              alt="Avatar"
-              className="avatar avatar-48 bg-light rounded-circle text-white p-2"
-              src={userData?.image ?? ""}
-              // style={{ marginLeft: "0.5rem" }}
-            />
-            {userData?.name ?? "Unknown"}
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-function AddNewContact() {
-  return (
-    <button
-      className="btn btn-success"
-      style={{ position: "fixed", bottom: "5%", right: "4%" }}
-    >
-      New Contact
-      <svg
-        style={{ marginLeft: "10px" }}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="#fff"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        className="feather feather-user-plus"
-      >
-        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-        <circle cx="8.5" cy="7" r="4"></circle>
-        <line x1="20" y1="8" x2="20" y2="14"></line>
-        <line x1="23" y1="11" x2="17" y2="11"></line>
-      </svg>
-    </button>
   );
 }
 

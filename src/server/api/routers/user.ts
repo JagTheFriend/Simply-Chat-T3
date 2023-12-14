83afd8ac90bf583e1fcba45fc7381b196ed07d6e
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-14 11:34:29 +0200

    Added new route to find user by email
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,12 +2,22 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  findUser: protectedProcedure
+  findUserById: protectedProcedure
     .input(z.object({ userId: z.string() }))
     .query(async ({ ctx, input }) => {
       return await ctx.db.user.findUnique({
         where: {
           id: input.userId,
+        },
+      });
+    }),
+
+  findUserByEmail: protectedProcedure
+    .input(z.object({ email: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.user.findUnique({
+        where: {
+          email: input.email,
         },
       });
     }),

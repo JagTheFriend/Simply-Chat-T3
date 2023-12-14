a195972f97b42bf9899d72f203d1f3dfc3931416
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-14 21:31:51 +0200

    Display go back button and contact's profile
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/chat/[contactId].tsx
+++ src/pages/chat/[contactId].tsx
@@ -1,9 +1,32 @@
 import type { User } from "@prisma/client";
 import { useRouter } from "next/router";
 import { createContext } from "react";
+import UserProfile from "~/components/Profile";
 import { api } from "~/utils/api";
 
 const ContactDetailsContext = createContext({} as User);
+
+function GoBack() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#000"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="feather feather-arrow-left"
+      >
+        <line x1="19" y1="12" x2="5" y2="12"></line>
+        <polyline points="12 19 5 12 12 5"></polyline>
+      </svg>
+    </>
+  );
+}
 
 function LoadUserData({ contactId }: { contactId: string }) {
   const { data, isError } = api.user.findUserById.useQuery(
@@ -20,7 +43,13 @@ function LoadUserData({ contactId }: { contactId: string }) {
 
   return (
     <ContactDetailsContext.Provider value={data}>
-      hello
+      <div
+        style={{ display: "flex", alignItems: "center", paddingTop: "1rem" }}
+      >
+        <GoBack />
+        <UserProfile username={data.name} avatar={data.image} />
+      </div>
+      <hr />
     </ContactDetailsContext.Provider>
   );
 }
@@ -33,7 +62,7 @@ export default function Chat() {
 
   return (
     <div className="container" style={{ fontSize: "21px" }}>
-      <LoadUserData contactId={contactId} />;
+      <LoadUserData contactId={contactId} />
     </div>
   );
 }

47f0f991dea1b8b24c0f5197f0ac54098e76d9f5
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-14 21:28:14 +0200

    Created component to display user profile
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function UserProfile({
+  username,
+  avatar,
+}: {
+  username: string | null | undefined;
+  avatar: string | null | undefined;
+}) {
+  return (
+    <div style={{ fontSize: "21px" }}>
+      <Image
+        alt="Avatar"
+        className="avatar avatar-48 bg-light rounded-circle text-white p-2"
+        src={avatar ?? ""}
+        style={{ marginLeft: "0.5rem" }}
+        width={"100"}
+        height={"100"}
+      />{" "}
+      {username}!
+    </div>
+  );
+}

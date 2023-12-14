2352bc8197970eb29f1df6d8fa45ef60ba52b46f
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-14 11:20:57 +0200

    Display modal
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,32 +1,78 @@
 import Image from "next/image";
 import { api } from "~/utils/api";
 
+function AddNewContactModal() {
+  return (
+    <div
+      className="modal fade"
+      id="addNewContactModal"
+      tabIndex={-1}
+      aria-labelledby="addNewContactLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="addNewContactLabel">
+              Add New Contact
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">...</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-success">
+              Add Contact
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function AddNewContact() {
   return (
-    <button
-      className="btn btn-success"
-      style={{ position: "fixed", bottom: "5%", right: "4%" }}
-    >
-      New Contact
-      <svg
-        style={{ marginLeft: "10px" }}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="#fff"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        className="feather feather-user-plus"
+    <>
+      <button
+        className="btn btn-success"
+        style={{ position: "fixed", bottom: "5%", right: "4%" }}
+        data-bs-toggle="modal"
+        data-bs-target="#addNewContactModal"
       >
-        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-        <circle cx="8.5" cy="7" r="4"></circle>
-        <line x1="20" y1="8" x2="20" y2="14"></line>
-        <line x1="23" y1="11" x2="17" y2="11"></line>
-      </svg>
-    </button>
+        New Contact
+        <svg
+          style={{ marginLeft: "10px" }}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#fff"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-user-plus"
+        >
+          <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+          <circle cx="8.5" cy="7" r="4"></circle>
+          <line x1="20" y1="8" x2="20" y2="14"></line>
+          <line x1="23" y1="11" x2="17" y2="11"></line>
+        </svg>
+      </button>
+      <AddNewContactModal />
+    </>
   );
 }
 

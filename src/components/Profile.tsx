85b264e631b7c8a28b22bdb2de524e3fb8cbe51a
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-15 10:36:18 +0200

    Use dicebear to generate profile image
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,5 @@
+import { identicon } from "@dicebear/collection";
+import { createAvatar } from "@dicebear/core";
 import Image from "next/image";
 
 export default function UserProfile({
@@ -7,17 +9,21 @@ export default function UserProfile({
   username: string | null | undefined;
   avatar: string | null | undefined;
 }) {
+  const generatedAvatar = createAvatar(identicon, {
+    seed: username ?? "Unknown",
+  });
+
   return (
     <div style={{ fontSize: "21px" }}>
       <Image
         alt="Avatar"
         className="avatar avatar-48 bg-light rounded-circle text-white p-2"
-        src={avatar ?? ""}
+        src={avatar ?? generatedAvatar.toDataUriSync()}
         style={{ marginLeft: "0.5rem" }}
         width={"100"}
         height={"100"}
       />{" "}
-      {username}
+      {username ?? "Unknown"}
     </div>
   );
 }

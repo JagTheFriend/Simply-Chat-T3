02197f8c22120f58421d1df28ad4f6ae8baef2c6
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-15 10:26:55 +0200

    Fixing errors
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Contact/Modal.tsx
+++ src/components/Contact/Modal.tsx
@@ -101,9 +101,9 @@ export function AddNewContact() {
           viewBox="0 0 24 24"
           fill="none"
           stroke="#fff"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="feather feather-user-plus"
         >
           <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>

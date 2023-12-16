d9fc7e01815ad545d94300ce0e73d3456d02ab9b
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-16 09:52:43 +0200

    Export type to interface
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -7,10 +7,11 @@ export const config = {
   },
 };
 
-const ioHandler = (
-  req: NextApiRequest,
-  res: NextApiResponse & { socket: { server: { io: ServerIO } } }
-) => {
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: { server: { io: ServerIO } & NetServer };
+};
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
     const path = "/api/socket/io";
     const httpServer = res.socket.server as NetServer;

bc466c386927c7ea85f9aadd6f41bf993ace5cb4
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-18 10:37:03 +0200

    Created message in the database
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/api/socket/message/index.ts
+++ src/pages/api/socket/message/index.ts
@@ -1,5 +1,9 @@
 import type { NextApiRequest } from "next";
-import type { NextApiResponseWithSocketIo } from "~/utils/types";
+import { api } from "~/utils/api";
+import type {
+  NewMessageData,
+  NextApiResponseWithSocketIo,
+} from "~/utils/types";
 
 export default function handler(
   req: NextApiRequest,
@@ -11,6 +15,11 @@ export default function handler(
   try {
     // Creating a message
     if (req.method == "POST") {
+      const data = req.body as NewMessageData;
+      api.message.createMessage.useMutation().mutate({
+        content: data.content,
+        receiverId: data.receiverId,
+      });
       res?.socket?.server?.io?.emit("newMessage", req.body);
       res.status(200).json({ message: "New Message Sent" });
     }

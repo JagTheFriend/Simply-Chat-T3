e69be3edc6aad307cfe91ebfeb7b611fe14f0721
JagTheFriend <JagTheFriend12@gmail.com>
2023-12-18 10:20:53 +0200

    Emit newMessage
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/api/socket/message/index.ts
+++ src/pages/api/socket/message/index.ts
@@ -11,7 +11,8 @@ export default function handler(
   try {
     // Creating a message
     if (req.method == "POST") {
-      console.log(req.body);
+      res?.socket?.server?.io?.emit("newMessage", req.body);
+      res.status(200).json({ message: "New Message Sent" });
     }
 
     // Deleting a message
